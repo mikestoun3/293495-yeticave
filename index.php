9d534c71f1df0ad1edc18ef02d5ec0242f7f58b8
mikestoun3 <mikestoun3@gmail.com>
2017-04-20 17:19:15 +0700

    Исправлен слайлгайд и фото+имя пользователя

--- index.php
+++ index.php
@@ -27,25 +27,25 @@ $user_avatar = 'img/user.jpg';
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-     <?php if ($is_auth == true) { ?>
-        <div class="user-menu__image">
-    <img src="" width="40" height="40" alt="Пользователь">
-  </div>
-  <div class="user-menu__logged">
-    <p></p>
-  </div>
-<?php } else { ?>
-    <ul class="user-menu__list">
-    <li class="user-menu__item">
-      <a href="#">Регистрация</a>
-    </li>
-    <li class="user-menu__item">
-      <a href="#">Вход</a>
-    </li>
-  </ul>
-<?php } ?>
-
+        <?php if ($is_auth == true) { ?>
+          <div class="user-menu__image">
+            <img src="<?php print($user_avatar) ?>" width="40" height="40" alt="Пользователь">
+          </div>
+          <div class="user-menu__logged">
+            <p>
+            <?php print($user_name) ?>
+            </p>
+          </div>
+          <?php } else { ?>
+          <ul class="user-menu__list">
+            <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+            </li>
+            <li class="user-menu__item">
+              <a href="#">Вход</a>
+            </li>
+          </ul>
+          <?php } ?>
         </nav>
     </div>
 </header>
