a2439136be28b1b0ca72d3ef05bfd9399e15ff23
mikestoun3 <mikestoun3@gmail.com>
2017-04-21 13:33:21 +0700

    Изменение на альтернативный син

--- index.php
+++ index.php
@@ -26,26 +26,24 @@ $user_avatar = 'img/user.jpg';
         </form>
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
-        <nav class="user-menu">
-        <?php if ($is_auth == true) { ?>
-          <div class="user-menu__image">
-            <img src="<?php print($user_avatar) ?>" width="40" height="40" alt="Пользователь">
-          </div>
-          <div class="user-menu__logged">
-            <p>
-            <?php print($user_name) ?>
-            </p>
-          </div>
-          <?php } else { ?>
-          <ul class="user-menu__list">
-            <li class="user-menu__item">
-              <a href="#">Регистрация</a>
-            </li>
-            <li class="user-menu__item">
-              <a href="#">Вход</a>
-            </li>
-          </ul>
-          <?php } ?>
+         <nav class="user-menu">
+            <?php if ($is_auth == true): ?>
+            <div class="user-menu__image">
+                <img src="<?php print($user_avatar)?>" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p><?php print($user_name)?></p>
+            </div>
+            <?php else: ?>
+            <ul class="user-menu__list">
+              <li class="user-menu__item">
+                 <a href="#">Регистрация</a>
+              </li>
+              <li class="user-menu__item">
+                <a href="#">Вход</a>
+              </li>
+            </ul>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
