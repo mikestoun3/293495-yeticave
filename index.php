ef249607eb099b5b72558c325a7743e0313c69c5
mikestoun3 <mikestoun3@gmail.com>
2017-04-21 21:09:56 +0700

    исправлен 0 перед минутами

--- index.php
+++ index.php
@@ -16,7 +16,11 @@ $lot_time_difference = ($tomorrow - $now);
 $lot_hours_remaining = floor($lot_time_difference/3600);
 $lot_mins_remaining = floor(($lot_time_difference/60)-$lot_hours_remaining*60);
 $lot_time_remaining = $lot_hours_remaining .":". $lot_mins_remaining;
-?>
+if($lot_mins_remaining<10){
+$lot_time_remaining = $lot_hours_remaining .":0". $lot_mins_remaining;
+  } else {
+$lot_time_remaining= $lot_hours_remaining .":".$lot_mins_remaining;
+}
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -97,7 +101,7 @@ $lot_time_remaining = $lot_hours_remaining .":". $lot_mins_remaining;
                             <span class="lot__cost">10 999<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
+                            <?= $lot_time_remaining;?>
                         </div>
                     </div>
                 </div>
