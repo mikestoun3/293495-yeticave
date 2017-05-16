d5570b52a993daab2d66c1ccd078b9e8aaea7e0b
mikestoun3 <mikestoun3@gmail.com>
2017-05-16 17:45:46 +0700

    templating

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+// функция обеспечивает защиту от XSS
+function dataFiltering($data)
+{
+    if (is_array($data)) {
+        $result = [];
+        foreach ($data as $key => $value) {
+            $result[$key] = dataFiltering($value);
+        }
+    } else {
+        $result = htmlspecialchars($data);
+    }
+    return $result;
+}
+// функция выводит шаблон из заданного файла
+function includeTemplate($file, $data)
+{
+    if (file_exists($file)) {
+        $data = dataFiltering($data);
+        extract($data);
+        ob_start();
+        include($file);
+        $result = ob_get_clean();
+    } else {
+        $result = "";
+    }
+    return $result;
+}
+// функция выводит время в относительном формате
+function relativeTime($ts)
+{
+    $minutes = (time() - $ts) / 60;
+    $hours = $minutes / 60;
+    if ($hours > 24) {
+        $result = gmdate("d.m.y в H:i", $ts);
+    } else if ($minutes > 60) {
+        $result = (int) $hours." часов назад";
+    } else {
+        $result = (int) $minutes." минут назад";
+    }
+    return $result;
+}
+?>
