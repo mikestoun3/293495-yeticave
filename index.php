458ca41a938ee28bf98913e51e9f4b6ae6b0b034
mikestoun3 <mikestoun3@gmail.com>
2017-05-17 16:32:11 +0700

    IDs for lots

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 // подключение файла с функциями
 require_once 'functions.php';
+// подключение файла с данными лотов
+require_once 'lots_data.php';
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
 // записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
@@ -21,15 +23,7 @@ if ($lot_mins_remaining < 10) {
 }
 // массив категорий
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-// массив объявлений
-$lots = [
-    ["name" => "2014 Rossignol District Snowboard", "category" => "Доски и лыжи", "price" => "10999", "image" => "img/lot-1.jpg"],
-    ["name" => "DC Ply Mens 2016/2017 Snowboard", "category" => "Доски и лыжи", "price" => "159999", "image" => "img/lot-2.jpg"],
-    ["name" => "Крепления Union Contact Pro 2015 года размер L/XL", "category" => "Крепления", "price" => "8000", "image" => "img/lot-3.jpg"],
-    ["name" => "Ботинки для сноуборда DC Mutiny Charocal", "category" => "Ботинки", "price" => "10999", "image" => "img/lot-4.jpg"],
-    ["name" => "Куртка для сноуборда DC Mutiny Charocal", "category" => "Одежда", "price" => "7500", "image" => "img/lot-5.jpg"],
-    ["name" => "Маска Oakley Canopy", "category" => "Разное", "price" => "5400", "image" => "img/lot-6.jpg"]
-];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
--- lot.php
+++ lot.php
@@ -1,6 +1,26 @@
 <?php
-
+// подключение файла с функциями
 require_once 'functions.php';
+
+// подключение файла с данными лотов
+require_once 'lots_data.php';
+
+// проверка, что есть get-параметр id, если нет то 404
+if (!isset($_GET['id'])) {
+    header("HTTP/1.0 404 Not Found");
+    echo "id is required";
+    exit;
+}
+
+// если передали несуществующий id, то тоже 404
+$lotId = $_GET['id'];
+if (!isset($lots[$lotId])) {
+    header("HTTP/1.0 404 Not Found");
+    echo "Bad id";
+    exit;
+}
+
+$current_lot = $lots[$lotId];
 
 // ставки пользователей, которыми надо заполнить таблицу
 $bets = [
@@ -9,13 +29,14 @@ $bets = [
     ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
     ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
 ];
+
 ?>
 
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>DC Ply Mens 2016/2017 Snowboard</title>
+    <title><?=$current_lot['name']?></title>
     <link href="css/normalize.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
@@ -23,7 +44,7 @@ $bets = [
 
 <?=includeTemplate('templates/lot_header.php', []); ?>
 
-<?=includeTemplate('templates/lot_main.php', ['bets' => $bets]); ?>
+<?=includeTemplate('templates/lot_main.php', ['lot' => $current_lot, 'bets' => $bets]); ?>
 
 <?=includeTemplate('templates/footer.php', []); ?>
 
