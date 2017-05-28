50656e51422fc8d74085e311d15fe2685b37854a
mikestoun3 <mikestoun3@gmail.com>
2017-05-28 19:04:14 +0700

    test2

--- add.php
+++ add.php
@@ -5,60 +5,66 @@ require_once 'lots_data.php';
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"]; 
 
 // если заполнено поле, если поле заполнено правильно 
-$data_new_lot = []; 
+
 
 // не заполнено, неправильно заполнено 
 $data_error = []; 
 
-//проверка имени 
-if (!empty($_POST['lot-name'])) { 
-$data_new_lot['name'] = htmlspecialchars($_POST['lot-name']); 
-} else { 
-$data_error['name'] = 'Введите наименование лота'; 
-} 
+if(isset($_POST['form_submit'])) {
 
-//проверка описания 
-if (!empty($_POST['message'])) { 
-$data_new_lot['description'] = htmlspecialchars($_POST['message']); 
-} else { 
-$data_error['message'] = 'Введите описание лота'; 
-} 
+    //проверка имени 
+    if (!empty($_POST['lot-name'])) { 
+    $data_new_lot['name'] = htmlspecialchars($_POST['lot-name']); 
+    } else { 
+    $data_error['name'] = 'Введите наименование лота'; 
+    } 
 
-//проверка начальной стоимости 
-if (empty($_POST['lot-rate']) || !is_numeric($_POST['lot-rate'])) { 
-$data_error['lot-rate'] = 'Некорректное значение'; 
-} else { 
-$data_new_lot['price'] = $_POST['lot-rate']; 
-} 
+    //проверка описания 
+    if (!empty($_POST['message'])) { 
+    $data_new_lot['description'] = htmlspecialchars($_POST['message']); 
+    } else { 
+    $data_error['message'] = 'Введите описание лота'; 
+    } 
 
-//проверка шага стоимости 
-if (empty($_POST['lot-step']) || !is_numeric($_POST['lot-step']) || $_POST['lot-step'] < 0) { 
-$data_error['lot-step'] = 'Некорректное значение'; 
-} else { 
-$data_new_lot['step'] = $_POST['lot-step']; 
-} 
+    //проверка начальной стоимости 
+    if (empty($_POST['lot-rate']) || !is_numeric($_POST['lot-rate'])) { 
+    $data_error['lot-rate'] = 'Некорректное значение'; 
+    } else { 
+    $data_new_lot['price'] = $_POST['lot-rate']; 
+    } 
 
-//проверка даты 
-if (empty($_POST['lot-date'])) { 
-$data_error['lot-date'] = 'Введите дату'; 
-} else { 
-$data_new_lot['date'] = $_POST['lot-date']; 
-} 
+    //проверка шага стоимости 
+    if (empty($_POST['lot-step']) || !is_numeric($_POST['lot-step']) || $_POST['lot-step'] < 0) { 
+    $data_error['lot-step'] = 'Некорректное значение'; 
+    } else { 
+    $data_new_lot['step'] = $_POST['lot-step']; 
+    } 
 
-//проверка категорий 
-if (!in_array($_POST['category'], $categories)) { 
-$data_error['category'] = 'Некорректное значение'; 
-} 
+    //проверка даты 
+    if (empty($_POST['lot-date'])) { 
+    $data_error['lot-date'] = 'Введите дату'; 
+    } else { 
+    $data_new_lot['date'] = $_POST['lot-date']; 
+    } 
 
-//проверка и размещение фото 
-if (isset($_FILES['lot-file'])) { 
-$file = $_FILES['lot-file']; 
-if ($file['type'] == 'image/jpeg' || $file['type'] == 'image/png') { 
-$uploadedfile = move_uploaded_file($file['tmp_name'], 'img/' . $file['name']); 
-} else { 
-$data_error['lot-file'] = 'Загрузите фото в формате jpeg/png'; 
-} 
-} 
+    //проверка категорий 
+    if (!in_array($_POST['category'], $categories)) { 
+    $data_error['category'] = 'Некорректное значение'; 
+    } else {
+      $data_new_lot['category'] = $_POST['category'];
+    }
+
+    //проверка и размещение фото 
+    if (isset($_FILES['lot-file'])) { 
+    $file = $_FILES['lot-file']; 
+    if ($file['type'] == 'image/jpeg' || $file['type'] == 'image/png') { 
+    $uploadedfile = move_uploaded_file($file['tmp_name'], 'img/' . $file['name']); 
+    } else { 
+    $data_error['lot-file'] = 'Загрузите фото в формате jpeg/png'; 
+    } 
+    } 
+}
+  $data_new_lot = []; 
 ?>
 
 
@@ -73,7 +79,11 @@ $data_error['lot-file'] = 'Загрузите фото в формате jpeg/pn
 <body> 
 
 <?=includeTemplate('templates/lot_header.php', []); ?> 
-<?=includeTemplate('templates/add_main.php', []); ?> 
+<? if (count($data_error) == 0) {
+  print includeTemplate('templates/add_main.php', $data_new_lot);
+} else {
+  print includeTemplate('templates/add_main.php', $data_error);
+} ?>
 <?=includeTemplate('templates/footer.php', []); ?> 
 
 </body> 
--- templates/add_main.php
+++ templates/add_main.php
@@ -1,9 +1,11 @@
-<?
+<?php
+include_once 'lots_data.php';
+include_once 'add.php';
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"]; 
-
 ?>
-<main class="main">
-   <nav class="nav">
+
+<main>
+  <nav class="nav">
     <ul class="nav__list container">
       <li class="nav__item">
         <a href="all-lots.html">Доски и лыжи</a>
@@ -25,64 +27,66 @@ $categories = ["Доски и лыжи", "Крепления", "Ботинки",
       </li>
     </ul>
   </nav>
-    <form class="form form--add-lot container  <?= $data['errors']? 'form--invalid': '' ?>" enctype="multipart/form-data" action="add.php" method="post">
-        <h2>Добавление лота</h2>
-        <div class="form__container-two">
-            <div class="form__item <?= $data['errors']['name']? 'form__item--invalid': '' ?>">
-                <label for="lot-name">Наименование</label>
-                <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
-                <span class="form__error"><?= $data['errors']['name'] ?></span>
-            </div>
-            <div class="form__item <?= $data['errors']['category']? 'form__item--invalid': '' ?>">
-                <label for="category">Категория</label>
-                <select id="category" name="category" required>
-                   <option>Все категории</option>
-                <?php foreach ($categories as $key => $value): ?>
-                    <option><?=$value;?></option>
-                <?php endforeach; ?>
-            </select>
-                <span class="form__error"><?= $data['errors']['category'] ?></span>
-            </div>
+  <form class="form form--add-lot container" action="add.php" method="post"> <!-- form--invalid -->
+    <h2>Добавление лота</h2>
+    <div class="form__container-two">
+      <div class="form__item"> <!-- form__item--invalid -->
+        <label for="lot-name">Наименование</label>
+        <input id="lot-name" type="text" name="lot-name" class="<?= ($data_error['name']) ? "form__item--invalid" : '' ?>" placeholder="Введите наименование лота">
+        <span class="form__error"></span>
+      </div>
+      <div class="form__item">
+        <label for="category">Категория</label>
+        <select id="category" name="category" required>
+          <option>Выберите категорию</option>
+          <option>Доски и лыжи</option>
+          <option>Крепления</option>
+          <option>Ботинки</option>
+          <option>Одежда</option>
+          <option>Инструменты</option>
+          <option>Разное</option>
+        </select>
+        <span class="form__error"></span>
+      </div>
+    </div>
+    <div class="form__item form__item--wide">
+      <label for="message">Описание</label>
+      <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
+      <span class="form__error"></span>
+    </div>
+    <div class="form__item form__item--file"> <!-- form__item--uploaded -->
+      <label>Изображение</label>
+      <div class="preview">
+        <button class="preview__remove" type="button">x</button>
+        <div class="preview__img">
+          <img src="../img/avatar.jpg" width="113" height="113" alt="Изображение лота">
         </div>
-        <div class="form__item form__item--wide <?= $data['errors']['message']? 'form__item--invalid': '' ?>">
-            <label for="message">Описание</label>
-            <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
-            <span class="form__error"><?= $data['errors']['message'] ?></span>
-        </div>
-        <div class="form__item form__item--file <?= $data['errors']['file']? 'form__item--invalid': '' ?>">
-            <label>Изображение</label>
-            <div class="preview">
-                <button class="preview__remove" type="button">x</button>
-                <div class="preview__img">
-                    <img src="img/avatar.jpg" width="113" height="113" alt="Изображение лота">
-                </div>
-            </div>
-            <div class="form__input-file">
-                <input class="visually-hidden" type="file" name="image" id="photo2">
-                <label for="photo2">
-                    <span>+ Добавить</span>
-                </label>
-            </div>
-            <span class="form__error"><?= $data['errors']['file'] ?></span>
-        </div>
-        <div class="form__container-three">
-            <div class="form__item form__item--small <?= $data['errors']['lot-rate']? 'form__item--invalid': '' ?>">
-                <label for="lot-rate">Начальная цена</label>
-                <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
-                <span class="form__error"><?= $data['errors']['lot-rate'] ?></span>
-            </div>
-            <div class="form__item form__item--small <?= $data['errors']['lot-step']? 'form__item--invalid': '' ?>">
-                <label for="lot-step">Шаг ставки</label>
-                <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
-                <span class="form__error"><?= $data['errors']['lot-step'] ?></span>
-            </div>
-            <div class="form__item <?= $data['errors']['lot-date']? 'form__item--invalid': '' ?>">
-                <label for="lot-date">Дата завершения</label>
-                <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="20.05.2017" required>
-                <span class="form__error"><?= $data['errors']['lot-date'] ?></span>
-            </div>
-        </div>
-        <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
-        <button type="submit" class="button">Добавить лот</button>
-    </form>
+      </div>
+      <div class="form__input-file">
+        <input class="visually-hidden" type="file" id="photo2" value="">
+        <label for="photo2">
+          <span>+ Добавить</span>
+        </label>
+      </div>
+    </div>
+    <div class="form__container-three">
+      <div class="form__item form__item--small">
+        <label for="lot-rate">Начальная цена</label>
+        <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
+        <span class="form__error"></span>
+      </div>
+      <div class="form__item form__item--small">
+        <label for="lot-step">Шаг ставки</label>
+        <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
+        <span class="form__error"></span>
+      </div>
+      <div class="form__item">
+        <label for="lot-date">Дата заверщения</label>
+        <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="20.05.2017" required>
+        <span class="form__error"></span>
+      </div>
+    </div>
+    <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <button type="submit" class="button">Добавить лот</button>
+  </form>
 </main>
